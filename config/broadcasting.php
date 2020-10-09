1f83a5d86095abe053812994921c9a9f8b7718e2
Dave Mills <d.e.mills@stats4sd.org>
2020-10-09 19:50:10 +0100

    adding websockets and vue scaffolding

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,13 +37,29 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
+                'encrypted' => true,
+                'host' => '127.0.0.1',
+                'port' => env('PUSHER_PORT', 6001),
+                'scheme' => 'http',
             ],
         ],
 
-        'redis' => [
-            'driver' => 'redis',
-            'connection' => 'default',
+        'pusher_valet' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
+            'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'encrypted' => true,
+                'host' => '127.0.0.1',
+                'port' => env('PUSHER_PORT', 6001),
+                'scheme' => 'https',
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ],
+            ],
         ],
 
         'log' => [
---
+++ resources/js/components/Comms.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container full-height d-flex justify-content-center align-items-center">
+        <h1 style="height: 40px">
+            You're early.
+        </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
